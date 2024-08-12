3150c492c969bb3105a8ec591b698ca2f4d63365
RandomProgrammerGuy <parsa_farjam@yahoo.com>
2024-08-12 14:48:54 +0200

    integrated auto-redirect for French and English

--- redirect.js
+++ redirect.js
@@ -1,2 +1,15 @@
 // THIS SCRIPT IS USED TO AUTOMATICALLY REDIRECT THE USER TO THE CORRECT PAGE BASED ON THEIR BROWSER LANGUAGE
 
+const lang = navigator.language || navigator.userLanguage || navigator.browserLanguage || "fallback";
+const metas = document.getElementsByTagName("meta")
+
+
+if (lang.indexOf("fr") > -1) {
+    metas.refresh.content = "0; url=./fr/accueil.html"
+} else {
+    metas.refresh.content = "0; url=./en/home.html"
+}
+
+metas.refresh.setAttribute("http-equiv", "refresh")
+
+console.log(metas.refresh)
